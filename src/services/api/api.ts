5331632046cd96981c7be3bd798f2b5dbe91c940
Andrey Grachev <andreysgra@gmail.com>
2026-03-09 20:41:18 +0300

    Добавляет интерфейс работы с сервером.

---
+++ src/services/api/api.ts
@@ -0,0 +1,29 @@
+import axios, {AxiosError, AxiosInstance, InternalAxiosRequestConfig} from 'axios';
+import {BASE_URL, REQUEST_TIMEOUT} from './const';
+import {getToken} from '../token';
+import {ResponseError} from './type';
+
+export const createApi = (): AxiosInstance => {
+  const api = axios.create({
+    baseURL: BASE_URL,
+    timeout: REQUEST_TIMEOUT
+  });
+
+  api.interceptors.request.use(
+    (config: InternalAxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    });
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError<ResponseError>) => Promise.reject(error)
+  );
+
+  return api;
+};
