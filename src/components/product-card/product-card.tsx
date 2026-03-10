e416e43544c26e1fab93fb5d59ea5ffb1dd617ba
Andrey Grachev <andreysgra@gmail.com>
2026-03-10 14:55:59 +0300

    Создаёт универсальный компонент карточки товара и подключает его к странице каталога. Удаляет компонент «CatalogList».

---
+++ src/components/product-card/product-card.tsx
@@ -0,0 +1,60 @@
+import {Link} from 'react-router-dom';
+import classNames from 'classnames';
+import {TProduct} from '../../types/product';
+import {AppRoute, RouteParam} from '../../const';
+
+type ProductCardProps = {
+  product: TProduct;
+  isBig?: boolean;
+}
+
+function ProductCard({product, isBig}: ProductCardProps) {
+  const {
+    id,
+    title,
+    price,
+    previewImage,
+    previewImageWebp,
+    isNew,
+    isFavorite
+  } = product;
+
+  const link = AppRoute.Product.replace(RouteParam.Id, id);
+
+  return (
+    <div className={classNames('card-item', {'card-item--big': isBig})}>
+      <Link className="card-item__img-link" to={link}>
+        <div className="card-item__img-wrapper">
+          <picture>
+            <source
+              type="image/webp"
+              srcSet={`${previewImageWebp}, ${previewImageWebp} 2x`}
+            />
+            <img
+              src={previewImage}
+              srcSet={`${previewImage} 2x`}
+              width={326}
+              height={332}
+              alt={title}
+            />
+          </picture>
+        </div>
+        {isNew && <span className="card-item__label">Новинка</span>}
+      </Link>
+      <button className={classNames('card-item__favorites', {'card-item__favorites--active': isFavorite})}>
+        <span className="visually-hidden">Добавить в избранное</span>
+        <svg width={51} height={41} aria-hidden="true">
+          <use xlinkHref="#icon-like" />
+        </svg>
+      </button>
+      {isBig && <span className="card-item__price">{price} p</span>}
+      <Link className="card-item__link" to={link}>
+        <h3 className="card-item__title">
+          <span>{title}</span>
+        </h3>
+      </Link>
+    </div>
+  );
+}
+
+export default ProductCard;
