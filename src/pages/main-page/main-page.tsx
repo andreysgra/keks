bf7bbcdea5de4e69e743026bf511d9d6bdd2a5eb
Andrey Grachev <andreysgra@gmail.com>
2026-03-10 14:13:31 +0300

    Создаёт компонент «Хиро» и подключает его к главной странице.

--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,33 +1,13 @@
 import Header from '../../components/header/header';
 import Footer from '../../components/footer/footer';
+import Hero from '../../components/hero/hero';
 
 function MainPage() {
   return (
     <div className="wrapper">
       <Header />
       <main>
-        <div className="hero">
-          <div className="container">
-            <div className="hero__img-wrapper">
-              <img
-                className="hero__img"
-                src="img/svg/hero-keks.svg"
-                width={727}
-                height={569}
-                alt="Картика кота."
-              />
-            </div>
-            <div className="hero__wrapper">
-              <p className="hero__subtitle">Твоя пушистая кондитерская</p>
-              <p className="hero__title">КЕКС</p>
-              <div className="hero__button-wrapper">
-                <a className="btn" href="catalog-page.html">
-                  Скорее смотреть
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Hero />
         <section className="random-main">
           <div className="container">
             <h2 className="random-main__title">кексы</h2>
