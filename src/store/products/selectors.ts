21d408f7a316e78a8befe24ae5b979ba8c5363bb
Andrey Grachev <andreysgra@gmail.com>
2026-03-10 12:24:23 +0300

    Создаёт компонент списка товаров.

--- src/store/products/selectors.ts
+++ src/store/products/selectors.ts
@@ -2,3 +2,6 @@ import {State} from '../../types/state';
 import {StoreSlice} from '../const';
 
 export const getProducts = (state: State) => state[StoreSlice.Products].products;
+
+export const getIsProductsLoading =
+  (state: State) => state[StoreSlice.Products].isProductsLoading;
