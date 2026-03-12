df1255289062c0fba3c2924f2536324b8046051f
Andrey Grachev <andreysgra@gmail.com>
2026-03-12 18:44:06 +0300

    Создаёт компонент с подробным описанием товара и подключает его к странице товара.

--- src/pages/product-page/product-page.tsx
+++ src/pages/product-page/product-page.tsx
@@ -1,126 +1,19 @@
 import Footer from '../../components/footer/footer';
 import Header from '../../components/header/header';
+import {useParams} from 'react-router-dom';
+import Product from '../../components/product/product';
+import BackLink from '../../components/back-link/back-link';
 
 function ProductPage() {
+  const id = useParams().id as string;
+
   return (
     <div className="wrapper">
       <Header />
       <main>
         <h1 className="visually-hidden">Карточка: пользователь не авторизован</h1>
-        <div className="back-link">
-          <div className="container">
-            <a className="back-link__link" href="#">
-              Назад
-              <svg
-                className="back-link__icon"
-                width={30}
-                height={16}
-                aria-hidden="true"
-              >
-                <use xlinkHref="#icon-arrow-left" />
-              </svg>
-            </a>
-          </div>
-        </div>
-        <section className="item-details">
-          <div className="container">
-            <div className="item-details__wrapper">
-              <div className="item-details__top-wrapper">
-                <h2 className="item-details__name">Чизкейк Лимонный</h2>
-                <span className="item-details__price">4 100 р</span>
-              </div>
-              <div className="item-details__weight-wrapper">
-                <span className="item-details__weight">1 300 грамм</span>
-              </div>
-              <div className="item-details__bottom-wrapper">
-                <div className="item-details__image-wrapper">
-                  <picture>
-                    <source
-                      type="image/webp"
-                      srcSet="img/content/lemon-pie.webp, img/content/lemon-pie@2x.webp 2x"
-                    />
-                    <img
-                      src="img/content/lemon-pie.jpg"
-                      srcSet="img/content/lemon-pie@2x.jpg 2x"
-                      width={241}
-                      height={245}
-                      alt="Чизкейк лимонный"
-                    />
-                  </picture>
-                  <span className="item-details__label">Новинка</span>
-                </div>
-                <div className="item-details__review-wrapper">
-                  <div className="star-rating star-rating--big">
-                    <svg
-                      className="star-rating__star star-rating__star--active"
-                      width={30}
-                      height={30}
-                      aria-hidden="true"
-                    >
-                      <use xlinkHref="#icon-star" />
-                    </svg>
-                    <svg
-                      className="star-rating__star star-rating__star--active"
-                      width={30}
-                      height={30}
-                      aria-hidden="true"
-                    >
-                      <use xlinkHref="#icon-star" />
-                    </svg>
-                    <svg
-                      className="star-rating__star star-rating__star--active"
-                      width={30}
-                      height={30}
-                      aria-hidden="true"
-                    >
-                      <use xlinkHref="#icon-star" />
-                    </svg>
-                    <svg
-                      className="star-rating__star star-rating__star--active"
-                      width={30}
-                      height={30}
-                      aria-hidden="true"
-                    >
-                      <use xlinkHref="#icon-star" />
-                    </svg>
-                    <svg
-                      className="star-rating__star star-rating__star--active"
-                      width={30}
-                      height={30}
-                      aria-hidden="true"
-                    >
-                      <use xlinkHref="#icon-star" />
-                    </svg>
-                    <span className="star-rating__count">26</span>
-                  </div>
-                  <div className="item-details__text-wrapper">
-                    <span className="item-details__text">
-                  Цитрусовый десерт с&nbsp;тонким сливочным вкусом, лёгкой
-                  свежестью и&nbsp;низким содержанием калорий&nbsp;сд
-                    </span>
-                    <button className="item-details__more">
-                      <span className="visually-hidden">Читать полностью</span>
-                      <svg width={27} height={17} aria-hidden="true">
-                        <use xlinkHref="#icon-more" />
-                      </svg>
-                    </button>
-                  </div>
-                  <div className="item-details__button-wrapper">
-                    <button className="item-details__like-button">
-                      <svg width={45} height={37} aria-hidden="true">
-                        <use xlinkHref="#icon-like" />
-                      </svg>
-                      <span className="visually-hidden">Понравилось</span>
-                    </button>
-                    <button className="btn btn--second" type="button">
-                      Оставить отзыв
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </section>
+        <BackLink />
+        <Product id={id} />
         <div className="filter-sort">
           <div className="container">
             <div className="filter-sort__inner">
