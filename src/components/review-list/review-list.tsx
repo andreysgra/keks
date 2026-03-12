4ec98af3998ef138dbddfd5cd6b4f44b42090220
Andrey Grachev <andreysgra@gmail.com>
2026-03-12 21:44:52 +0300

    Подключает кнопку «Показать ещё» в компоненте списка отзывов.

--- src/components/review-list/review-list.tsx
+++ src/components/review-list/review-list.tsx
@@ -6,6 +6,10 @@ import {useEffect} from 'react';
 import {fetchReviews} from '../../store/reviews/api-actions';
 import Review from '../review/review';
 import Loader from '../loader/loader';
+import ShowMoreButton from '../show-more-button/show-more-button';
+import {getReviewsCount} from '../../store/site-process/selectors';
+import {setReviewsCount} from '../../store/site-process/slice';
+import {REVIEWS_PER_LOAD} from '../../const';
 
 type ReviewListProps = {
   id: TReview['id'];
@@ -14,6 +18,7 @@ type ReviewListProps = {
 function ReviewList({id}: ReviewListProps) {
   const reviews = useAppSelector(getReviews);
   const isReviewsLoading = useAppSelector(getIsReviewsLoading);
+  const displayedComments = useAppSelector(getReviewsCount);
 
   const dispatch = useAppDispatch();
 
@@ -21,19 +26,26 @@ function ReviewList({id}: ReviewListProps) {
     dispatch(fetchReviews(id));
   }, [id, dispatch]);
 
+  useEffect(() => {
+    dispatch(setReviewsCount(REVIEWS_PER_LOAD));
+  }, [dispatch]);
+
   if (isReviewsLoading) {
     return <Loader />;
   }
 
+  const handleShowMoreButtonClick = () =>
+    dispatch(setReviewsCount(displayedComments + REVIEWS_PER_LOAD));
+
+  const isShowMoreButtonShowed = reviews.length > displayedComments;
+
   return (
     <div className="container">
       <div className="comments__wrapper">
-        {reviews.map((review) => <Review key={review.id} review={review} />)}
+        {reviews.slice(0, displayedComments).map((review) => <Review key={review.id} review={review} />)}
       </div>
       <div className="comments__show-more">
-        <button className="btn btn--second comments__button" type="button">
-          Показать еще
-        </button>
+        <ShowMoreButton onClick={handleShowMoreButtonClick} isComments isShowed={isShowMoreButtonShowed} />
       </div>
     </div>
   );
