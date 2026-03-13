68b322ef2b0b4e8f932471cc107123b4a4cadafb
Andrey Grachev <andreysgra@gmail.com>
2026-03-13 18:04:46 +0300

    Создаёт компонент показа сообщения об отсутствии отзывов.

---
+++ src/components/no-reviews/no-reviews.tsx
@@ -0,0 +1,19 @@
+function NoReviews() {
+  return (
+    <section className="empty-results">
+      <div className="container">
+        <div className="empty-results__wrapper">
+          <h2 className="empty-results__title">
+            Про этот кекс нам ничего не&nbsp;рассказали. Вы&nbsp;можете оставить
+            свой отзыв первым.
+          </h2>
+          <svg width={180} height={166} aria-hidden="true">
+            <use xlinkHref="#icon-cake" />
+          </svg>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default NoReviews;
