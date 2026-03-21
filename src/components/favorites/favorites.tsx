5d2bc239286e5bc1f1a4ea01af73435da0eae7ec
Andrey Grachev <andreysgra@gmail.com>
2026-03-21 20:16:25 +0300

    Переносит блок количества товаров в компонент избранного.

--- src/components/favorites/favorites.tsx
+++ src/components/favorites/favorites.tsx
@@ -6,6 +6,9 @@ import {fetchFavorites} from '../../store/favorites/api-actions';
 import CatalogList from '../catalog-list/catalog-list';
 import Loader from '../loader/loader';
 import FavoritesEmpty from '../favorites-empty/favorites-empty';
+import {Link} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import {getFormattedNumber, getPlural} from '../../utils/utils';
 
 function Favorites() {
   const products = useAppSelector(getFavorites);
@@ -25,25 +28,50 @@ function Favorites() {
     return <FavoritesEmpty />;
   }
 
+  const productsTotalAmount =
+    products.reduce((amount, product) => amount + product.price, 0);
+
+  const formattedText = getPlural(products.length, ['кекс', 'кекса', 'кексов']);
+  const formattedAmount = getFormattedNumber(productsTotalAmount);
+
   return (
-    <section className="favourites">
-      <div className="container">
-        <h2 className="visually-hidden">Избранные товары</h2>
-        <div className="favourites__button">
-          <button className="btn btn--second" type="button">
-            Очистить
-          </button>
-        </div>
-      </div>
-      <section className="catalog">
+    <>
+      <section className="number-of-favourites favorites-page__qty">
         <div className="container">
-          <h2 className="visually-hidden">Каталог</h2>
-          <div className="catalog__wrapper">
-            <CatalogList products={products} />
+          <h2 className="visually-hidden">Количество товаров в избранном.</h2>
+          <p className="number-of-favourites__cakes">{products.length} {formattedText}</p>
+          <div className="number-of-favourites__wrapper">
+            <div className="number-of-favourites__wrap-price">
+              <p className="number-of-favourites__text">Всего</p>
+              <p className="number-of-favourites__price">{formattedAmount}&nbsp;р</p>
+            </div>
+          </div>
+          <div className="number-of-favourites__button">
+            <Link className="btn" to={AppRoute.Catalog}>
+              В каталог
+            </Link>
           </div>
         </div>
       </section>
-    </section>
+      <section className="favourites">
+        <div className="container">
+          <h2 className="visually-hidden">Избранные товары</h2>
+          <div className="favourites__button">
+            <button className="btn btn--second" type="button">
+              Очистить
+            </button>
+          </div>
+        </div>
+        <section className="catalog">
+          <div className="container">
+            <h2 className="visually-hidden">Каталог</h2>
+            <div className="catalog__wrapper">
+              <CatalogList products={products}/>
+            </div>
+          </div>
+        </section>
+      </section>
+    </>
   );
 }
 
--- src/pages/favorites-page/favorites-page.tsx
+++ src/pages/favorites-page/favorites-page.tsx
@@ -11,23 +11,6 @@ function FavoritesPage() {
         <div className="favorites-page">
           <h1 className="visually-hidden">Избранное</h1>
           <BackLink />
-          <section className="number-of-favourites favorites-page__qty">
-            <div className="container">
-              <h2 className="visually-hidden">Количество товаров в избранном.</h2>
-              <p className="number-of-favourites__cakes">2 кекса</p>
-              <div className="number-of-favourites__wrapper">
-                <div className="number-of-favourites__wrap-price">
-                  <p className="number-of-favourites__text">Всего</p>
-                  <p className="number-of-favourites__price">13&nbsp;400&nbsp;р</p>
-                </div>
-              </div>
-              <div className="number-of-favourites__button">
-                <a className="btn" href="catalog.html">
-                  В каталог
-                </a>
-              </div>
-            </div>
-          </section>
           <Favorites />
         </div>
       </main>
