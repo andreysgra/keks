18299c1379d3138f3dd8b6ea66f9dd83781c1ff6
Andrey Grachev <andreysgra@gmail.com>
2026-03-24 18:14:30 +0300

    Удаляет неиспользуемые типы.

--- src/types/filter.ts
+++ src/types/filter.ts
@@ -1,11 +1,6 @@
-import {ProductCategoryName, ProductTypeName, ReviewsRating} from '../const';
+import {ReviewsRating} from '../const';
 import {TReviews} from './review';
-import {TProducts} from './product';
 
 export type TFilterOption = keyof typeof ReviewsRating
 
 export type TFilterReviews = Record<TFilterOption, (reviews: TReviews) => TReviews>
-
-export type TFilterByCategoryProducts = Record<keyof typeof ProductCategoryName, (products: TProducts) => TProducts>
-
-export type TFilterByTypeProducts = Record<keyof typeof ProductTypeName, (products: TProducts) => TProducts>
